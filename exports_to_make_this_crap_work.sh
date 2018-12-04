eeb3dad9c577ea49facae200d7eadddcc0d79f09
Trenton Ford <tford9@lsu.edu>
2018-12-04 00:22:28 -0500

    fix

--- exports_to_make_this_crap_work.sh
+++
@@ -1,4 +0,0 @@
-# location of bin stuff
-export PATH=$PATH:~/Desktop/KERNAL_PROJECT_SPACE/usr/bin/
-# add dylibs to the execspace 
-export DYLD_LIBRARY_PATH=/Users/trentonford/Desktop/KERNAL_PROJECT_SPACE/usr/lib:DYLD_LIBRARY_PATH
--- s_types.h
+++
@@ -1,6 +0,0 @@
-#ifndef S_TYPES
-#define S_TYPES
-
-typedef unsigned int uint;
-
-#endif /* S_TYPES */ 
